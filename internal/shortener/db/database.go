b2f05724b400e60761f39d064f261c55a2e491b0
Alex Borisov <borisov@aeroidea.ru>
2023-10-08 14:38:46 +0300

    iter 10 create database

--- internal/shortener/db/database.go
+++ internal/shortener/db/database.go
@@ -7,6 +7,13 @@ import (
 	"time"
 )
 
+// #MENTOR: Стоит ли делать здесь DatabaseInterface?
+// А это как конкретная реализация - PGDatabase
+// В этом случае придется переопределять все методы sql.DB
+// Если здесь сделать интерфейс - можно будет протестить pingHandler через мок
+
+// #MENTOR: sql.DB позволяет писать любые запросы. Например, CREATE DATABASE IF NOT EXISTS.
+// Но для postgres не работает IF NOT EXISTS и выведет ошибку
 func InitializeDatabase(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
